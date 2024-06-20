39fe809ab523dc2f22c02f090083e099d06a3853
niyatir2608 <niyati.ranpara@techholding.co>
2024-06-20 14:18:12 +0530

    feat: add deduplication_scope

--- sqs.tf
+++ sqs.tf
@@ -16,6 +16,7 @@ resource "aws_sqs_queue" "sqs" {
   visibility_timeout_seconds  = var.visibility_timeout_seconds
   fifo_queue                  = var.fifo_queue
   content_based_deduplication = var.content_based_deduplication
+  deduplication_scope = var.deduplication_scope
   delay_seconds               = var.delay_seconds
   message_retention_seconds   = var.message_retention_seconds
   receive_wait_time_seconds   = var.receive_wait_time_seconds
